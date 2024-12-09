1715370dcd1386e91305e170351f6f6ba057f88e
Rajudas575 <rajudas575@gmail.com>
2024-12-09 10:50:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 // Data for the quiz
 const questions = [
   {
-    question: "What is the capital of France?",
+    question: 'What is the capital of France?',
     choices: ["Paris", "London", "Berlin", "Madrid"],
     answer: "Paris",
   },
   {
-    question: "What is the highest mountain in the world?",
+    question: 'What is the highest mountain in the world?',
     choices: ["Everest", "Kilimanjaro", "Denali", "Matterhorn"],
     answer: "Everest",
   },
   {
-    question: "What is the largest country by area?",
+    question: 'What is the largest country by area?',
     choices: ["Russia", "China", "Canada", "United States"],
     answer: "Russia",
   },
   {
-    question: "Which is the largest planet in our solar system?",
+    question: 'Which is the largest planet in our solar system?',
     choices: ["Earth", "Jupiter", "Mars"],
     answer: "Jupiter",
   },
   {
-    question: "What is the capital of Canada?",
+    question: 'What is the capital of Canada?',
     choices: ["Toronto", "Montreal", "Vancouver", "Ottawa"],
     answer: "Ottawa",
   },
